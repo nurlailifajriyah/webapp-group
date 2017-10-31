32408b390b7582d7a076ab1ab8d1d249cd2c678d
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2017-10-30 22:09:50 -0400

    Fix audio recorder

--- src/webapps/tempo/urls.py
+++ src/webapps/tempo/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',tempo.views.activate, name='activate')
-
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',tempo.views.activate, name='activate'),
+    url(r'^audio_recorder$', tempo.views.audio_recorder, name="audio_recorder"),
 
 ]
